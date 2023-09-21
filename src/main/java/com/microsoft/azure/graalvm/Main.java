db2cd38e34c93a1540b7eafd0f822ab30dadf5a3
Weidong Xu <weidxu@microsoft.com>
2023-09-21 15:54:16 +0800

    update test

--- src/main/java/com/microsoft/azure/graalvm/Main.java
+++ src/main/java/com/microsoft/azure/graalvm/Main.java
@@ -5,6 +5,7 @@ import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
+import com.azure.core.management.exception.ManagementException;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.identity.DefaultAzureCredentialBuilder;
@@ -27,7 +28,9 @@ public class Main {
 
         String rgName = "rg1-weidxu";
 
+        // a case for CRUD
         try {
+            System.out.println("create container group");
             ContainerGroup containerGroup = manager.containerGroups()
                     .define("container1-weidxu")
                     .withRegion(Region.US_WEST3)
@@ -38,15 +41,30 @@ public class Main {
                     .withContainerInstance("nginx", 80)
                     .withNewVirtualNetwork("10.0.0.0/24")
                     .create();
-            System.out.println("container group created: " + containerGroup.id());
+            System.out.println("container group created: " + containerGroup.name());
 
             manager.containerGroups().listByResourceGroup(rgName)
-                    .forEach(cg -> System.out.println("list container group: " + cg.id()));
+                    .forEach(cg -> System.out.println("list container group: " + cg.name()));
+
+            containerGroup.update()
+                    .withTag("use", "testing")
+                    .apply();
+            System.out.println("container group updated: " + containerGroup.name());
+
+            containerGroup = manager.containerGroups().getByResourceGroup("rg1-weidxu", "container1-weidxu");
+            System.out.println("get container group: " + containerGroup.name());
 
             manager.containerGroups().deleteById(containerGroup.id());
-            System.out.println("container group created: " + containerGroup.id());
+            System.out.println("container group deleted: " + containerGroup.name());
         } finally {
             manager.resourceManager().resourceGroups().deleteByName(rgName);
+        }
+
+        // a case for exception
+        try {
+            manager.containerGroups().getByResourceGroup("rg1-weidxu", "container2-not-exist");
+        } catch (ManagementException e) {
+            System.out.println("expected exception: " + e.getValue().getCode());
         }
     }
 }
