6ba9b3d42214cffce2c53fe37bb0cb858d6f3d2a
Weidong Xu <weidxu@microsoft.com>
2023-09-21 15:09:51 +0800

    add config

--- src/main/java/com/microsoft/azure/graalvm/Main.java
+++ src/main/java/com/microsoft/azure/graalvm/Main.java
@@ -4,10 +4,12 @@ import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.resourcemanager.containerinstance.ContainerInstanceManager;
+import com.azure.resourcemanager.containerinstance.models.ContainerGroup;
 
 public class Main {
 
@@ -23,8 +25,28 @@ public class Main {
                 .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
                 .authenticate(credential, profile);
 
-        long count = manager.containerGroups().list().stream().count();
+        String rgName = "rg1-weidxu";
 
-        System.out.println("count: " + count);
+        try {
+            ContainerGroup containerGroup = manager.containerGroups()
+                    .define("container1-weidxu")
+                    .withRegion(Region.US_WEST3)
+                    .withNewResourceGroup(rgName)
+                    .withLinux()
+                    .withPublicImageRegistryOnly()
+                    .withoutVolume()
+                    .withContainerInstance("nginx", 80)
+                    .withNewVirtualNetwork("10.0.0.0/24")
+                    .create();
+            System.out.println("container group created: " + containerGroup.id());
+
+            manager.containerGroups().listByResourceGroup(rgName)
+                    .forEach(cg -> System.out.println("list container group: " + cg.id()));
+
+            manager.containerGroups().deleteById(containerGroup.id());
+            System.out.println("container group created: " + containerGroup.id());
+        } finally {
+            manager.resourceManager().resourceGroups().deleteByName(rgName);
+        }
     }
 }
